d83c5b3cf288b7dfb7dc128c2f7c17797e03a33c
roboflover <96958044+roboflover@users.noreply.github.com>
2022-04-06 04:39:11 +0300

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,49 @@ let formSaveButton = formElement.querySelector('.form__button');
 let nameInput = formElement.querySelector('#form__name');
 let jobInput = formElement.querySelector('#form__caption');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+const cardsContainer = document.querySelector('.elements');
+
+function addCard(cardTitle, cardImage) {
+    const songTemplate = document.querySelector('#element-template').content;
+    const songElement = songTemplate.querySelector('.element').cloneNode(true);
+    songElement.querySelector('.element__image').src = cardImage;
+    songElement.querySelector('.element__title').textContent = cardTitle;
+  
+    cardsContainer.append(songElement);
+}
+
+initialCards.forEach(function (item) {
+    addCard(item.name, item.link);
+})
+
+  
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
